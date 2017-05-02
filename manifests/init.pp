d7d01b61017f0298b6e95c309b85f5ce222a8403
Nate Riffe <inkblot@movealong.org>
2017-05-02 07:11:50 -0500

    Use the new `hashicorp::download` for terraform

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,48 +1,10 @@
 # ex: syntax=puppet si sw=2 ts=2 et
 class terraform (
   $version,
-  $base_url      = 'https://releases.hashicorp.com/terraform',
-  $target_dir    = '/usr/local/bin',
-  $bin_name      = 'terraform',
-  $tmp_dir       = '/tmp',
-  $checksum_type = undef,
-  $checksum      = undef
+  $target_dir = '/usr/local/bin',
 ) {
-
-  case $::kernel {
-    'Linux': { $_os = 'linux' }
-    'FreeBSD': { $_os ='freebsd' }
-    'OpenBSD': { $_os = 'openbsd' }
-    'Darwin': { $_os = 'darwin' }
-    default: { fail("Unknown kernel ${::kernel}") }
-  }
-
-  case $::architecture {
-    'amd64', 'x86_64': { $_arch = 'amd64' }
-    'i386', 'i486', 'i586', 'x86': { $_arch = '386' }
-    default: { fail("Unknown architecture: ${::architecture}") }
-  }
-
-  $archive_filename = "terraform_${version}_${_os}_${_arch}.zip"
-  archive { "${tmp_dir}/${archive_filename}":
-    ensure        => present,
-    checksum      => $checksum,
-    checksum_type => $checksum_type,
-    extract       => true,
-    extract_path  => $tmp_dir,
-    source        => "${base_url}/${version}/${archive_filename}",
-    creates       => "${target_dir}/${bin_name}-${version}",
-  }
-
-  exec { '/usr/local/bin/terraform-version':
-    command     => "mv ${tmp_dir}/${bin_name} ${target_dir}/${bin_name}-${version}",
-    subscribe   => Archive["${tmp_dir}/${archive_filename}"],
-    path        => ['/bin'],
-    refreshonly => true,
-  }
-
-  file { "${target_dir}/${bin_name}":
-    ensure => link,
-    target => "${target_dir}/${bin_name}-${version}",
+  hashicorp::download { 'terraform':
+    version    => $version,
+    target_dir => $target_dir,
   }
 }
