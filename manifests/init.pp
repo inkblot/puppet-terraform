4c83e77f3f74488cd3f0131c4143995b5ff302a7
Ben Gray <ben.gray@graymeta.com>
2017-10-17 14:02:05 -0700

    Remove target_dir that is no longer used in download module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,8 @@
 # ex: syntax=puppet si sw=2 ts=2 et
 class terraform (
   $version,
-  $target_dir = '/usr/local/bin',
 ) {
   hashicorp::download { 'terraform':
     version    => $version,
-    target_dir => $target_dir,
   }
 }
